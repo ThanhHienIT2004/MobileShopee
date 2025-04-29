afac4febfa2218ab280818b317eb1b82bce8fae0
Huy2304 <116505781+Huy2304@users.noreply.github.com>
2025-04-29 15:08:30 +0700

    .

---
+++ MobileShopee/Db/DbConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace MobileShopee.Db
+{
+    public class DbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["MobileShopee"].ConnectionString;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
